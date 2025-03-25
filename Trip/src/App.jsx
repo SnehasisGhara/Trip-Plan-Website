f6817ace878996ffec715c496cc6e3c5d8e4c4fb
SnehasisGhara <snehasisgharaofficial@gmail.com>
2025-03-25 16:17:53 +0530

    toaster

--- Trip/src/App.jsx
+++ Trip/src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Hero from './Components/Customs/Hero'
 import Header from "./Components/Customs/Header";
 import Index from './Components/Create_trip/Index'
-import { Toaster } from './Components/ui/Toaster'
+import { Toaster } from './Components/ui/toaster'
 import Trip from './Components/View_trip/Trip'
 import { CartProvider } from "./Components/Context/CartContext"
 // Corrected import path for HotelsCart
